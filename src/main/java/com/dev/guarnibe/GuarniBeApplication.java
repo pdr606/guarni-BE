a85a0c3e549542dfbcf8ece6451871c57efae356
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-12-07 01:32:17 -0300

    docs: initial setup

---
+++ src/main/java/com/dev/guarnibe/GuarniBeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.guarnibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarniBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuarniBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/guarnibe/GuarniBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.guarnibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarniBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
