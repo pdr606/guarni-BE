ff22d6a8e1bd115250fa44b96f4a5278eae06fac
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2023-12-12 13:19:23 -0300

    feat: create product service with mapper

--- src/main/java/com/dev/guarnibe/service/ProductServiceImp.java
+++ src/main/java/com/dev/guarnibe/service/ProductServiceImp.java
@@ -1,27 +1,50 @@
 package com.dev.guarnibe.service;
 
 import com.dev.guarnibe.dto.ProductDto;
+import com.dev.guarnibe.mapper.ProductMapper;
+import com.dev.guarnibe.model.ProductEntity;
+import com.dev.guarnibe.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class ProductServiceImp implements ProductService {
+
+    private final ProductRepository productRepository;
     @Override
     public void save(ProductDto data) {
-
+        existsById(data.id());
+        ProductEntity entity = ProductMapper.INSTANCE.toEntity(data);
+        productRepository.save(entity);
     }
 
     @Override
     public boolean delete(ProductDto data) {
-        return false;
+        existsById(data.id());
+        productRepository.deleteById(data.id());
+        return true;
     }
 
     @Override
     public List<ProductDto> getAll() {
-        return null;
+        List<ProductEntity> entityList = productRepository.findAll();
+        return ProductMapper.INSTANCE.toDtoList(entityList);
     }
 
     @Override
     public ProductDto getById(Long id) {
-        return null;
+        ProductEntity entity = productRepository.findById(id).orElseThrow(RuntimeException::new);
+        return ProductMapper.INSTANCE.toDto(entity);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        if(productRepository.existsById(id)){
+            return true;
+        }
+        throw new RuntimeException();
     }
 }
